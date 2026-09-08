d7f0f7265cc85abd99af9fb80e590f01d7fb3d5e
Leolimasx <leonardolima080903@gmail.com>
2026-09-08 00:40:19 -0300

    Atualizando layout.tsx com metadados e favicon personalizado

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Grupo Fábrica de Doces | Sobremesa Mineira',
-  description: 'A verdadeira tradição do doce mineiro. Conheça nossas marcas exclusivas e leve para os seus clientes o sabor artesanal feito com dedicação.',
+  description: 'A verdadeira tradição do doce mineiro. Conheça nossas marcas exclusivas e leve para os seus clientes o sabor industrial feito com rigor e dedicação.',
   icons: {
     icon: '/logo-sobremesa.png',
   },
