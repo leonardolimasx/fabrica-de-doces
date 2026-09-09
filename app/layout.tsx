987b085852b14fdad0c647a14d0d123054731ef1
Leolimasx <leonardolima080903@gmail.com>
2026-09-08 23:30:02 -0300

    Adicionando codigo de verificacao do Google Search Console

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,9 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Fábrica de Doces | Sobremesa Mineira',
   description: 'A verdadeira tradição do doce mineiro. Conheça nossas marcas exclusivas e leve para os seus clientes o sabor industrial feito com rigor e dedicação.',
-  // APAGUE TODA A PARTE DE ICONS AQUI
+  other: {
+    'google-site-verification': 'GT2TZ0GwU8BmpBTkiZpjub4KFukZEHS11jc',
+  },
 }
 
 export default function RootLayout({
