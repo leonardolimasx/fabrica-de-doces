72e22adfd06a7757a397bfcd360096191342fbd0
Leolimasx <leonardolima080903@gmail.com>
2026-09-08 23:33:07 -0300

    Atualizando token de verificacao do Google

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   title: 'Fábrica de Doces | Sobremesa Mineira',
   description: 'A verdadeira tradição do doce mineiro. Conheça nossas marcas exclusivas e leve para os seus clientes o sabor industrial feito com rigor e dedicação.',
   other: {
-    'google-site-verification': 'GT2TZ0GwU8BmpBTkiZpjub4KFukZEHS11jc',
+    'google-site-verification': 'GT2TZ0GwU8BmpBTkiZpjub4KFukZEHS11jc5H0yA6tM',
   },
 }
 
